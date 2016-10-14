639c67581aedf8760debb431837f570ae07da902
myimay15@naver.com <myimay15@naver.com>
2016-10-14 11:26:38 +0900

    mv.go

--- mv.go
+++ mv.go
@@ -6,12 +6,6 @@ import (
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-
 	err := os.Rename(os.Args[1], os.Args[2])
 	if err != nil {
 		fmt.Println(err)
